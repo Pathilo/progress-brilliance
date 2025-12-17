7ad8c7ac6682f99021c5d187b0847fad74a4fada
Pathilo <113499827+Pathilo@users.noreply.github.com>
2025-12-17 15:14:25 +0100

    Update vite.config.ts
    Ustaw base w Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/progress-brilliance/",
+});
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
