e4349ac68b10aac3f6ad1a03f0bea9ea4f824b3a
Pathilo <113499827+Pathilo@users.noreply.github.com>
2025-12-17 15:59:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import OfferWindows from "./pages/OfferWindows";
@@ -18,29 +20,35 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/oferta/okna" element={<OfferWindows />} />
-          <Route path="/oferta/drzwi" element={<OfferDoors />} />
-          <Route path="/oferta/bramy" element={<OfferGarage />} />
-          <Route path="/oferta/zaluzje" element={<OfferBlinds />} />
-          <Route path="/oferta/rolety" element={<OfferRollers />} />
-          <Route path="/realizacje" element={<Realizations />} />
-          <Route path="/montaz" element={<Installation />} />
-          <Route path="/o-nas" element={<About />} />
-          <Route path="/kontakt" element={<Contact />} />
-          <Route path="/polityka-prywatnosci" element={<PrivacyPolicy />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  useEffect(() => {
+    document.title = "Progress Brilliance";
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/oferta/okna" element={<OfferWindows />} />
+            <Route path="/oferta/drzwi" element={<OfferDoors />} />
+            <Route path="/oferta/bramy" element={<OfferGarage />} />
+            <Route path="/oferta/zaluzje" element={<OfferBlinds />} />
+            <Route path="/oferta/rolety" element={<OfferRollers />} />
+            <Route path="/realizacje" element={<Realizations />} />
+            <Route path="/montaz" element={<Installation />} />
+            <Route path="/o-nas" element={<About />} />
+            <Route path="/kontakt" element={<Contact />} />
+            <Route path="/polityka-prywatnosci" element={<PrivacyPolicy />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
